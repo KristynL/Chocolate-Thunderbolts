6a2cbc851a2757f9b11c67d5ef315884074495bd
KristynL <klee248@gmail.com>
2016-03-04 15:53:47 -0800

    Update lab5.c

--- lab5.c
+++ lab5.c
@@ -12,6 +12,28 @@
 #define zerocross0 //reference zero cross
 #define zerocross1
 
+// ANSI colors
+#define	COLOR_BLACK		0
+#define	COLOR_RED		1
+#define	COLOR_GREEN		2
+#define	COLOR_YELLOW		3
+#define	COLOR_BLUE		4
+#define	COLOR_MAGENTA		5
+#define	COLOR_CYAN		6
+#define	COLOR_WHITE		7
+
+// Some ANSI escape sequences
+#define CURSOR_ON "\x1b[?25h"
+#define CURSOR_OFF "\x1b[?25l"
+#define CLEAR_SCREEN "\x1b[2J"
+#define GOTO_YX "\x1B[%d;%dH"
+#define CLR_TO_END_LINE "\x1B[K"
+
+/* Black foreground, white background */
+#define BKF_WTB "\x1B[0;30;47m"
+#define FORE_BACK "\x1B[0;3%d;4%dm"
+#define FONT_SELECT "\x1B[%dm"
+
 // C8051F381_ADC_multiple_inputs.c:  Shows how to use the 10-bit ADC and the
 // multiplexer.  This program measures the voltages applied to pins P2.0 to P2.3.
 //
@@ -83,6 +105,34 @@ char _c51_external_startup (void)
 	
 	return 0;
 }
+
+void PORT_Init (void)
+{
+	P0MDOUT |= 0x10; // Enable UTX as push-pull output
+	XBR0     = 0x01; // Enable UART on P0.4(TX) and P0.5(RX)                     
+	XBR1     = 0x40; // Enable crossbar and weak pull-ups
+}
+
+void SYSCLK_Init (void)
+{
+	CLKSEL|=0b_0000_0011; // SYSCLK derived from Internal HF Osc / 1.
+	OSCICN |= 0x03;   // Configure internal oscillator for its maximum 
+	RSTSRC  = 0x04;   // Enable missing clock detector
+}
+ 
+void UART0_Init (void)
+{
+	SCON0 = 0x10;
+	TH1 = 0x10000-((SYSCLK/BAUDRATE)/2L);
+	CKCON &= ~0x0B; // T1M = 1; SCA1:0 = xx
+	CKCON |=  0x08;
+	TL1 = TH1;      // Init Timer1
+	TMOD &= ~0xf0;  // TMOD: timer 1 in 8-bit auto-reload
+	TMOD |=  0x20;                       
+	TR1 = 1; // START Timer1
+	TI = 1;  // Indicate TX0 ready
+}
+ 
 
 // Uses Timer3 to delay <us> micro-seconds. 
 void Timer3us(unsigned char us)
